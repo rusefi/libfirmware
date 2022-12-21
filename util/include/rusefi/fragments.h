663174265d842cbd38c096023c750ab68e74221f
Andrey Gusakov <dron0gus@gmail.com>
2022-12-21 12:36:00 +0300

    fragments: getRangePtr() helper for memcpy-less access to fragments data

--- util/include/rusefi/fragments.h
+++ util/include/rusefi/fragments.h
@@ -49,3 +49,6 @@ struct FragmentList {
 
 // copy `size` of fragmented outputs in to destination, skipping the first `skip` bytes
 void copyRange(uint8_t* destination, FragmentList src, size_t skip, size_t size);
+// returns pointer to actual data and size of contiguous data
+// if data is located in more than one fragmnet - returned value will be size available in first fragment
+size_t getRangePtr(uint8_t **ptr, FragmentList src, size_t offset, size_t size);
