c038614b003476a581187f3dbb2518a2be7c504a
rusefillc <sdfsdfqsf2334234234>
2023-07-09 14:29:57 -0400

    moving GDI4 to higher addresses

--- can/can_common.h
+++ can/can_common.h
@@ -1,3 +1,4 @@
 // We have a bit of a mess with too many useful usages of CAN and too little documentation at https://github.com/rusefi/rusefi/wiki/CAN
 
-#define GDI4_BASE_ADDRESS 0xF0
+// we use ExtID but only 16 bits in comms protocol from ECU to GDI4
+#define GDI4_BASE_ADDRESS 0xBB20
