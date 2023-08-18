cd215a965cca31aff98a18a786d5ad626e853b15
andreika-git <andrei.home@gmail.com>
2023-08-18 23:06:30 +0300

    boards_id.h

---
+++ board_id/boards_id.h
@@ -0,0 +1,49 @@
+//
+// was generated automatically by Hellen Board-ID generation tool gen_board_ids_h.py 2023-08-18 23:05:26.714402
+//
+
+#pragma once
+
+#define BOARD_ID_UNKNOWN			0
+#define BOARD_ID_72_NB2_E			1
+#define BOARD_ID_81HD_B			2
+#define BOARD_ID_ALPHA2CH_B			3
+#define BOARD_ID_ALPHA4CH_A			4
+#define BOARD_ID_ALPHA2CH_C			5
+#define BOARD_ID_NB1_C			6
+#define BOARD_ID_NA6_94_C			7
+#define BOARD_ID_NISSAN121_C			8
+#define BOARD_ID_ALPHA8CH_A			9
+#define BOARD_ID_154HYUNDAI_C			10
+#define BOARD_ID_ALPHA4CH_B			11
+#define BOARD_ID_GM_E67_A			12
+#define BOARD_ID_55BMW_A			13
+#define BOARD_ID_GM_E38_A			14
+#define BOARD_ID_72_NB2_F			15
+#define BOARD_ID_ALPHA4CH_C			100
+#define BOARD_ID_88BMW_B			101
+#define BOARD_ID_GM_E67_B			102
+#define BOARD_ID_ALPHA4CH_D			103
+#define BOARD_ID_NISSAN121_D			104
+#define BOARD_ID_ALPHA4CH_E			105
+#define BOARD_ID_NA6_94_D			106
+#define BOARD_ID_ALPHA2CH_D			107
+#define BOARD_ID_GM_E67_C			108
+#define BOARD_ID_NISSAN121_E			109
+#define BOARD_ID_HONDA125_A			110
+#define BOARD_ID_VAG121_D			111
+#define BOARD_ID_ALPHA2CH_E			112
+#define BOARD_ID_ALPHA4CH_F			113
+#define BOARD_ID_ALPHA4CH_H			114
+#define BOARD_ID_ALPHA2CH_F			115
+#define BOARD_ID_ALPHA8CH_B			200
+#define BOARD_ID_154HYUNDAI_D			201
+#define BOARD_ID_HONDA125_C			202
+#define BOARD_ID_ALPHA4CH_G			203
+#define BOARD_ID_HONDA125_B			204
+#define BOARD_ID_ALPHA4CH_I			205
+#define BOARD_ID_HONDA125_D			206
+#define BOARD_ID_HONDA125_E			207
+#define BOARD_ID_SMALL_CAN_A			208
+#define BOARD_ID_SMALL_CAN_B			209
+#define BOARD_ID_ALPHA2CH_G			210
