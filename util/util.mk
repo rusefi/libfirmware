03defc1513c37149ef5af894dbc8393f6a3603e8
andreika-git <andrei.home@gmail.com>
2023-08-18 23:23:06 +0300

    add board_id to the include path

--- util/util.mk
+++ util/util.mk
@@ -1,4 +1,4 @@
-RUSEFI_LIB_INC += $(RUSEFI_LIB)/util/include $(RUSEFI_LIB)/can
+RUSEFI_LIB_INC += $(RUSEFI_LIB)/util/include $(RUSEFI_LIB)/can $(RUSEFI_LIB)/board_id
 
 RUSEFI_LIB_CPP += \
 	$(RUSEFI_LIB)/util/src/util_dummy.cpp \
