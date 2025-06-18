5c0f1f9c137f7b8b81990f1df10bdb8d188c4398
Anton Lantcevich <awpe00@gmail.com>
2025-06-18 23:12:20 +0200

    min

--- util/include/rusefi/efistringutil.h
+++ util/include/rusefi/efistringutil.h
@@ -4,6 +4,10 @@
 
 #define TO_LOWER(x) (((x)>='A' && (x)<='Z') ? (x) - 'A' + 'a' : (x))
 
+extern "C" {
+#include <cstdlib>
+}
+
 int indexOf(const char *string, char ch);
 bool strEqualCaseInsensitive(const char *str1, const char *str2);
 bool strEqual(const char *str1, const char *str2);
