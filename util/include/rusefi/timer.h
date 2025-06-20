293f070b237b1e689121544fbcee09a7b5f61814
Anton Lantcevich <awpe00@gmail.com>
2025-06-20 13:12:09 +0200

    Timer fortify

--- util/include/rusefi/timer.h
+++ util/include/rusefi/timer.h
@@ -12,37 +12,39 @@
  */
 class Timer final {
 public:
-	Timer();
+	Timer() = default;
+
 	void reset();
 
 	// Reset the timer to a known timestamp (don't take a timestamp internally)
 	void reset(efitick_t nowNt);
+
 	// returns timer to the most original-as-constructed state
 	void init();
 
-	bool hasElapsedSec(float seconds) const;
-	bool hasElapsedMs(float ms) const;
-	bool hasElapsedUs(float us) const;
+	[[nodiscard]] bool hasElapsedSec(float seconds) const;
+	[[nodiscard]] bool hasElapsedMs(float ms) const;
+	[[nodiscard]] bool hasElapsedUs(float us) const;
 
 	// Return the elapsed time since the last reset.
 	// If the elapsed time is longer than 2^32 timer tick counts,
 	// then a time period representing 2^32 counts will be returned.
-	float getElapsedSeconds() const;
-	float getElapsedSeconds(efitick_t nowNt) const;
-	float getElapsedUs() const;
+	[[nodiscard]] float getElapsedSeconds() const;
+	[[nodiscard]] float getElapsedSeconds(efitick_t nowNt) const;
+	[[nodiscard]] float getElapsedUs() const;
 	// WOW yes returns US while parameter is NT
-	float getElapsedUs(efitick_t nowNt) const;
+	[[nodiscard]] float getElapsedUs(efitick_t nowNt) const;
 	// too many options for the API probably?
-	uint32_t getElapsedNt(efitick_t nowNt) const;
+	[[nodiscard]] uint32_t getElapsedNt(efitick_t nowNt) const;
 
 	// Perform an atomic update event based on the passed timestamp,
 	// returning the delta between the last reset and the provided timestamp
-	float getElapsedSecondsAndReset(efitick_t nowNt);
+	[[nodiscard]] float getElapsedSecondsAndReset(efitick_t nowNt);
 
-	efitick_t get() {
-	    return m_lastReset;
-	}
+	[[nodiscard]] efitick_t get() const;
+
+	static constexpr efitick_t InitialState = INT64_MIN / 8;
 
 private:
-	efitick_t m_lastReset;
+	efitick_t m_lastReset{InitialState};
 };
--- util/src/timer.cpp
+++ util/src/timer.cpp
@@ -1,10 +1,5 @@
-
 #include <rusefi/timer.h>
 #include <rusefi/rusefi_time_math.h>
-
-Timer::Timer() {
-	init();
-}
 
 void Timer::reset() {
 	m_lastReset = getTimeNowNt();
@@ -12,32 +7,42 @@ void Timer::reset() {
 
 void Timer::init() {
 	// Use not-quite-minimum value to avoid overflow
-	m_lastReset = INT64_MIN / 8;
+	m_lastReset = InitialState;
 }
 
-void Timer::reset(efitick_t nowNt) {
+void Timer::reset(efitick_t const nowNt) {
 	m_lastReset = nowNt;
 }
 
-bool Timer::hasElapsedSec(float seconds) const {
+bool Timer::hasElapsedSec(float const seconds) const {
 	return hasElapsedMs(seconds * 1000);
 }
 
-bool Timer::hasElapsedMs(float milliseconds) const {
+bool Timer::hasElapsedMs(float const milliseconds) const {
 	return hasElapsedUs(milliseconds * 1000);
 }
 
-bool Timer::hasElapsedUs(float microseconds) const {
-	auto delta = getTimeNowNt() - m_lastReset;
+bool Timer::hasElapsedUs(float const microseconds) const {
+	// Like past has already passed...
+	if (microseconds <= 0) {
+		return true;;
+	}
+
+	efitick_t const delta{ getTimeNowNt() - m_lastReset };
 
 	// If larger than 32 bits, timer has certainly expired
 	if (delta >= UINT32_MAX) {
 		return true;
 	}
 
-	auto delta32 = (uint32_t)delta;
+	constexpr float max_32_bit_fit_float{ 4294967295.f };
 
-	return delta32 > USF2NT(microseconds);
+	if (microseconds >= max_32_bit_fit_float) {
+		auto const ntDouble{ static_cast<double>(microseconds) * US_TO_NT_MULTIPLIER };
+		return delta >= static_cast<efitick_t>(ntDouble);
+	}
+
+	return static_cast<uint32_t>(delta) >= static_cast<uint32_t>(USF2NT(microseconds));
 }
 
 float Timer::getElapsedSeconds() const {
@@ -45,15 +50,15 @@ float Timer::getElapsedSeconds() const {
 }
 
 float Timer::getElapsedSeconds(efitick_t nowNt) const {
-	return 1 / US_PER_SECOND_F * getElapsedUs(nowNt);
+	return 1.f / US_PER_SECOND_F * getElapsedUs(nowNt);
 }
 
 float Timer::getElapsedUs() const {
 	return getElapsedUs(getTimeNowNt());
 }
 
-uint32_t Timer::getElapsedNt(efitick_t nowNt) const {
-	auto deltaNt = nowNt - m_lastReset;
+uint32_t Timer::getElapsedNt(efitick_t const nowNt) const {
+	int64_t const deltaNt{ nowNt - m_lastReset };
 
 	// Yes, things can happen slightly in the future if we get a lucky interrupt between
 	// the timestamp and this subtraction, that updates m_lastReset to what's now "the future",
@@ -62,22 +67,41 @@ uint32_t Timer::getElapsedNt(efitick_t nowNt) const {
 		return 0;
 	}
 
-	if (deltaNt > UINT32_MAX - 1) {
-		deltaNt = UINT32_MAX - 1;
+	return deltaNt >= static_cast<int64_t>(UINT32_MAX) ? UINT32_MAX : static_cast<uint32_t>(deltaNt);
+}
+
+float Timer::getElapsedUs(efitick_t const nowNt) const {
+	// Wraparound is not considered
+	auto const delta32{ getElapsedNt(nowNt) };
+
+	// One second at 168mhz is 168 mil of ticks
+	// float loses precision after 24bit ~ 16777216us;
+	// 16777216us / 1mil ~ 16.8s. So after 16s we lose precision
+	// On faster chips precision is lost proportionally
+	constexpr uint32_t max_exact_float_int{ 16777216 };
+	constexpr uint32_t max_exact_int{ max_exact_float_int * US_TO_NT_MULTIPLIER };
+
+	// Waited too long, calculate in double to keep precision
+	// It is not like someone will be unhappy seeing elapsed time being up to 250us higher/lower after 16s
+	// It is to prevent error accumulation somewhere
+	if (delta32 > max_exact_int) {
+		auto const delta32f{ static_cast<double>(delta32) };
+		return static_cast<float>(NT2US(delta32f));
 	}
-	return deltaNt;
+
+	// Ok less than 16s so can be float for faster calc while keeping precision
+	auto const delta32f{ static_cast<float>(delta32) };
+	return static_cast<float>(NT2US(delta32f));
 }
 
-float Timer::getElapsedUs(efitick_t nowNt) const {
-	auto delta32 = getElapsedNt(nowNt);
-
-	return NT2US(delta32);
-}
-
-float Timer::getElapsedSecondsAndReset(efitick_t nowNt) {
-	float result = getElapsedSeconds(nowNt);
+float Timer::getElapsedSecondsAndReset(efitick_t const nowNt) {
+	float const result{ getElapsedSeconds(nowNt) };
 
 	reset(nowNt);
 
 	return result;
+}
+
+[[nodiscard]] efitick_t Timer::get() const {
+	return m_lastReset;
 }
