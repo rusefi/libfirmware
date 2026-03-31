752d1c4ed4d09dcda738e43acab679c43973aee8
rusefillc <rusefillc@gmail.com>
2026-03-31 13:14:36 -0400

    can_common.h

--- can/can_common.h
+++ can/can_common.h
@@ -1,3 +1,5 @@
+// can_common.h
+
 // We have a bit of a mess with too many useful usages of CAN and too little documentation at https://github.com/rusefi/rusefi/wiki/CAN
 
 // we use ExtID but only 16 bits in comms protocol from ECU to GDI4
